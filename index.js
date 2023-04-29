497a259f9299cb025d12978528663e70882fc5de
Glauber Pinheiro <31114023+glauberspinheiro@users.noreply.github.com>
2023-04-29 01:56:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Chamar o modulo HTTP
+//var http = require("http");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+app.listen(8000, function(){
+    console.log('Exemple app listening on port http://localhost:8000!')
+})
